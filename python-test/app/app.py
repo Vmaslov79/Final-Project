70380b403534ef7702cea8eb3ac9b8d6348770f9
vlad <vmaslov79@gmail.com>
2025-09-19 13:51:23 +0000

    Final-project: create app.py file, this is test server it's chek code

---
+++ python-test/app/app.py
@@ -0,0 +1,15 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+class SimoleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b'OK')
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+if __name__ == '__main__':
+    server = HTTPServer(('0.0.0.0', 8080), SimoleHandler)
+    print("Server running on port 8080...")
+    server.serve_forever()
