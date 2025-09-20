4a5758eb4bf02770cd937c86676e55598ca51878
vlad <vmaslov79@gmail.com>
2025-09-20 15:26:02 +0000

    Final-project: block code from 12 to 28 string add check and out ip-address pod

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
+import socket
 from http.server import BaseHTTPRequestHandler, HTTPServer
-class SimoleHandler(BaseHTTPRequestHandler):
+class SimpleHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
             self.send_response(200)
@@ -8,8 +9,20 @@ class SimoleHandler(BaseHTTPRequestHandler):
         else:
             self.send_response(404)
             self.end_headers()
+def get_pod_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('10.40.10.10', 1))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+    return ip
 
 if __name__ == '__main__':
-    server = HTTPServer(('0.0.0.0', 8080), SimoleHandler)
+    pod_ip = get_pod_ip()
+    print(f"Pod IP address: {pod_ip}")
+    server = HTTPServer(('0.0.0.0', 8080), SimpleHandler)
     print("Server running on port 8080...")
     server.serve_forever()
